1c021d130ca0a700b91eda85368f33463534103d
bryankeller <kellerbryan19@gmail.com>
2025-01-23 21:55:44 -0800

    Add ppcskel

---
+++ broadway.mk
@@ -0,0 +1,12 @@
+ifeq ($(strip $(DEVKITAMATEUR)),)
+$(error "Set DEVKITAMATEUR in your environment.")
+endif
+
+PREFIX = $(DEVKITAMATEUR)/bin/powerpc-none-elf-
+
+CFLAGS = -mcpu=750 -m32 -mhard-float -mno-eabi -mno-sdata
+CFLAGS += -ffreestanding -ffunction-sections
+CFLAGS += -Wall -Wextra -O0 -pipe
+ASFLAGS =
+LDFLAGS = -mcpu=750 -m32 -n -z muldefs -nostartfiles -nodefaultlibs -Wl,-gc-sections
+
---
+++ common.mk
@@ -0,0 +1,50 @@
+AR = $(PREFIX)ar
+AS = $(PREFIX)as
+CC = $(PREFIX)gcc
+CXX = $(PREFIX)g++
+LD = $(PREFIX)ld
+OBJCOPY = $(PREFIX)objcopy
+RANLIB = $(PREFIX)ranlib
+STRIP = $(PREFIX)strip
+
+ifeq ($(NOMAPFILE),)
+LDFLAGS += -Wl,-Map,$(TARGET).map -Wl,--no-warn-rwx-segments
+endif
+
+ifneq ($(LDSCRIPT),)
+LDFLAGS += -Wl,-T$(LDSCRIPT)
+endif
+
+DEPDIR = .deps
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+	@echo "  LINK      $@"
+	@$(CC) $(LDFLAGS) $(OBJS) $(LIBS) -o $@
+
+ifneq ($(LDSCRIPT),)
+$(TARGET): $(LDSCRIPT)
+endif
+
+%.o: %.c
+	@echo "  COMPILE   $<"
+	@mkdir -p $(DEPDIR)
+	@$(CC) $(CFLAGS) $(DEFINES) -Wp,-MMD,$(DEPDIR)/$(*F).d,-MQ,"$@",-MP -c $< -o $@
+
+%.o: %.s
+	@echo "  ASSEMBLE  $<"
+	@$(CC) $(CFLAGS) $(DEFINES) $(ASFLAGS) -c $< -o $@
+
+%.o: %.S
+	@echo "  ASSEMBLE  $<"
+	@$(CC) $(CFLAGS) $(DEFINES) $(ASFLAGS) -c $< -o $@
+
+clean:
+	rm -rf $(DEPDIR)
+	rm -f $(TARGET) $(TARGET).map $(OBJS)
+
+-include $(DEPDIR)/*
+
+.PHONY: clean
+
