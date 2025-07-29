9dd4b7b310d0affd60b72adaeb03c1fb32bf7719
bryankeller <kellerbryan19@gmail.com>
2025-07-28 21:28:01 -0700

    Fix comment typo regarding memory range

--- src/device_tree.c
+++ src/device_tree.c
@@ -141,7 +141,7 @@ void build_device_tree() {
 
             u32 reg[4] = {
                 0x00000000, 0x01800000, // 24MB MEM1
-                0x10000000, 0x03400000  // 58MB MEM2 (upper 12MB used for IOS?)
+                0x10000000, 0x03400000  // 52MB MEM2 (upper 12MB used for IOS?)
             };
             add_property("reg", reg, sizeof(reg));
         }
